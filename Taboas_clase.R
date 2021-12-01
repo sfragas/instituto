d49cf30fa3cdf4241a0dfe69cd030527111acdf2
sfragas <sfragas72@outlook.com>
2021-12-01 20:02:15 +0100

    Terminada propuesta de informe favorable

--- Taboas_clase.R
+++ Taboas_clase.R
@@ -8,11 +8,13 @@ detach(package:plyr)
 
 #######################PARA ESO E BAC#########################################
 #"ESO3_PMAR",
-clase<-c("ESO1_A","ESO1_B","ESO1_C","ESO2_A","ESO2_B","ESO2_C","ESO3_A","ESO3_B","ESO3_PMAR","ESO4_A","ESO4_B","BAC1_A","BAC2_A","INFOFI1","INFOFI2","PEIEST1","PEIEST2")
+clase<-c("ESO1_A","ESO1_B","ESO1_C","ESO2_A","ESO2_B","ESO2_C","ESO3_A","ESO3_B","ESO3_PMAR","ESO4_A","ESO4_B","BAC1_A","BAC2_A")
 j=1
 resultados<-data.frame(Clase=character(),Aprobados=integer(),Suspensos_1_3=integer(),
                        Suspensos_4_6=integer(),
                        Suspensos_7_mas=integer(),
+                       Trimestre=factor(),
+                       N_alumnos=integer(),
                       stringsAsFactors=F)
 
 for(i in clase){
@@ -20,10 +22,11 @@ datos<-read.csv2(paste0(i,".csv"),header = T,encoding = "UTF-8")
 #datos<-read.csv2("ESO4_A.csv",header = T,encoding = "UTF-8")
 #Arregalmos el data frame
 datos$CUALIFICACIÓN<-as.numeric(datos$CUALIFICACIÓN)
+datos$Trimestre<-as.factor(substr(datos$Nº.AVALIACIÓN,1,1))
 datos<-datos%>%mutate(Nome_completo=paste(NOME,X1º.APELIDO,X2º.APELIDO),Cat_notas=case_when(CUALIFICACIÓN>=5~"Aprobado",CUALIFICACIÓN<5~"Suspenso"))%>%
-    select(Nome_completo,Cat_notas)%>%na.omit()
+    select(Nome_completo,Cat_notas,Trimestre)%>%na.omit()
 datos$Nome_completo<-as.factor(datos$Nome_completo)
-                   
+N_alumnos<-length(levels(datos$Nome_completo))                   
 datos1<-group_by(datos,Nome_completo)
 datos1<-datos1%>%summarise(N_suspensos=length(Cat_notas[Cat_notas=="Suspenso"]))
 datos1<-datos1%>%mutate(Casos=case_when(N_suspensos==0~"Aprobados",
@@ -32,8 +35,9 @@ datos1<-datos1%>%mutate(Casos=case_when(N_suspensos==0~"Aprobados",
 ungroup(datos1)
 datos1<-datos1%>%group_by(Casos)%>%summarise(Total=n())%>%pivot_wider(names_from = Casos,values_from=Total)
 datos1$Clase<-paste(i)
-datos1<-datos1[,c(ncol(datos1),1:ncol(datos1)-1)]
+#datos1<-datos1[,c(ncol(datos1),1:ncol(datos1)-1)]
 resultados[j,"Clase"]<-datos1[1,"Clase"]
+resultados[j,"N_alumnos"]<-N_alumnos
 if("Aprobados"%in% colnames(datos1)){
   resultados[j,"Aprobados"]<-datos1[1,"Aprobados"]
 } else {
@@ -56,14 +60,14 @@ if("Suspensos_7_mas"%in% colnames(datos1)){
 }
 j=j+1
 }
+resultados$Trimestre<-datos$Trimestre[1]
 write.csv2(resultados,"CLASES_ESO_BAC.csv")
 
-##PARA INFOFI#################################
-clase<-c("INFOFI1","INFOFI2")
+##PARA INFOFI E PEIEST#################################
+clase<-c("INFOFI1","INFOFI2","PEIEST1","PEIEST2")
 j=1
 resultados<-data.frame(Clase=character(),Aprobados=integer(),Suspensos_1_3=integer(),
-                       Suspensos_4_6=integer(),
-                       Suspensos_7_mas=integer(),
+                       Suspensos_4_mas=integer(),Trimestre=factor(),
                        stringsAsFactors=F)
 
 for(i in clase){
@@ -71,20 +75,22 @@ for(i in clase){
   #datos<-read.csv2("ESO4_A.csv",header = T,encoding = "UTF-8")
   #Arregalmos el data frame
   datos$CUALIFICACIÓN<-as.numeric(datos$CUALIFICACIÓN)
+  datos$Trimestre<-as.factor(substr(datos$Nº.AVALIACIÓN,1,1))
   datos<-datos%>%mutate(Nome_completo=paste(NOME,X1º.APELIDO,X2º.APELIDO),Cat_notas=case_when(CUALIFICACIÓN>=5~"Aprobado",CUALIFICACIÓN<5~"Suspenso"))%>%
-    select(Nome_completo,Cat_notas)%>%na.omit()
+    select(Nome_completo,Cat_notas,Trimestre)%>%na.omit()
   datos$Nome_completo<-as.factor(datos$Nome_completo)
-  
+  N_alumnos<-length(levels(datos$Nome_completo))
   datos1<-group_by(datos,Nome_completo)
   datos1<-datos1%>%summarise(N_suspensos=length(Cat_notas[Cat_notas=="Suspenso"]))
   datos1<-datos1%>%mutate(Casos=case_when(N_suspensos==0~"Aprobados",
                                           N_suspensos>=1&N_suspensos<=3~"Suspensos_1_3",
-                                          N_suspensos>=4&N_suspensos<=6~"Suspensos_4_6",TRUE~"Suspensos_7_mas"))
+                                          TRUE~"Suspensos_4_mas"))
   ungroup(datos1)
   datos1<-datos1%>%group_by(Casos)%>%summarise(Total=n())%>%pivot_wider(names_from = Casos,values_from=Total)
   datos1$Clase<-paste(i)
   datos1<-datos1[,c(ncol(datos1),1:ncol(datos1)-1)]
   resultados[j,"Clase"]<-datos1[1,"Clase"]
+  resultados[j,"N_alumnos"]<-N_alumnos
   if("Aprobados"%in% colnames(datos1)){
     resultados[j,"Aprobados"]<-datos1[1,"Aprobados"]
   } else {
@@ -95,67 +101,13 @@ for(i in clase){
   } else {
     resultados[j,"Suspensos_1_3"]<-0
   }
-  if("Suspensos_4_6"%in% colnames(datos1)){
-    resultados[j,"Suspensos_4_6"]<-datos1[1,"Suspensos_4_6"]
+  if("Suspensos_4_mas"%in% colnames(datos1)){
+    resultados[j,"Suspensos_4_mas"]<-datos1[1,"Suspensos_4_mas"]
   } else {
-    resultados[j,"Suspensos_4_6"]<-0
-  }
-  if("Suspensos_7_mas"%in% colnames(datos1)){
-    resultados[j,"Suspensos_7_mas"]<-datos1[1,"Suspensos_7_mas"]
-  }else{
-    resultados[j,"Suspensos_7_mas"]<-0
+    resultados[j,"Suspensos_4_mas"]<-0
   }
   j=j+1
 }
-write.csv2(resultados,"CLASES_INFOFI.csv")
+resultados$Trimestre<-datos$Trimestre[1]
+write.csv2(resultados,"CLASES_INFOFI_PEIEST.csv")
 
-#############################PARA PEIEST################################################################
-clase<-c("PEIEST1","PEIEST2")
-j=1
-resultados<-data.frame(Clase=character(),Aprobados=integer(),Suspensos_1_3=integer(),
-                       Suspensos_4_6=integer(),
-                       Suspensos_7_mas=integer(),
-                       stringsAsFactors=F)
-
-for(i in clase){
-  datos<-read.csv2(paste0(i,".csv"),header = T,encoding = "UTF-8")
-  #datos<-read.csv2("ESO4_A.csv",header = T,encoding = "UTF-8")
-  #Arregalmos el data frame
-  datos$CUALIFICACIÓN<-as.numeric(datos$CUALIFICACIÓN)
-  datos<-datos%>%mutate(Nome_completo=paste(NOME,X1º.APELIDO,X2º.APELIDO),Cat_notas=case_when(CUALIFICACIÓN>=5~"Aprobado",CUALIFICACIÓN<5~"Suspenso"))%>%
-    select(Nome_completo,Cat_notas)%>%na.omit()
-  datos$Nome_completo<-as.factor(datos$Nome_completo)
-  
-  datos1<-group_by(datos,Nome_completo)
-  datos1<-datos1%>%summarise(N_suspensos=length(Cat_notas[Cat_notas=="Suspenso"]))
-  datos1<-datos1%>%mutate(Casos=case_when(N_suspensos==0~"Aprobados",
-                                          N_suspensos>=1&N_suspensos<=3~"Suspensos_1_3",
-                                          N_suspensos>=4&N_suspensos<=6~"Suspensos_4_6",TRUE~"Suspensos_7_mas"))
-  ungroup(datos1)
-  datos1<-datos1%>%group_by(Casos)%>%summarise(Total=n())%>%pivot_wider(names_from = Casos,values_from=Total)
-  datos1$Clase<-paste(i)
-  datos1<-datos1[,c(ncol(datos1),1:ncol(datos1)-1)]
-  resultados[j,"Clase"]<-datos1[1,"Clase"]
-  if("Aprobados"%in% colnames(datos1)){
-    resultados[j,"Aprobados"]<-datos1[1,"Aprobados"]
-  } else {
-    resultados[j,"Aprobados"]<-0
-  }
-  if("Suspensos_1_3"%in% colnames(datos1)){
-    resultados[j,"Suspensos_1_3"]<-datos1[1,"Suspensos_1_3"]
-  } else {
-    resultados[j,"Suspensos_1_3"]<-0
-  }
-  if("Suspensos_4_6"%in% colnames(datos1)){
-    resultados[j,"Suspensos_4_6"]<-datos1[1,"Suspensos_4_6"]
-  } else {
-    resultados[j,"Suspensos_4_6"]<-0
-  }
-  if("Suspensos_7_mas"%in% colnames(datos1)){
-    resultados[j,"Suspensos_7_mas"]<-datos1[1,"Suspensos_7_mas"]
-  }else{
-    resultados[j,"Suspensos_7_mas"]<-0
-  }
-  j=j+1
-}
-write.csv2(resultados,"CLASES_TOTAL.csv")
--- evaluacion.R
+++ evaluacion.R
@@ -6,18 +6,15 @@ library(ggplot2)
 library(plyr)
 rm(list=ls())
 #Importamos los datos
-datos<-read_xlsx("curso_21_22.xlsx")
-#Formateamos los datos
-#datos$Curso<-as.factor(datos$Curso)
-datos$Letra<-as.factor(datos$Letra)
-datos<-datos%>%mutate(Clase=ifelse(!is.na(datos$Letra),paste0(Curso,"_",substr(Letra,1,4)),paste0(Curso)))
+datos<-read.csv2("CLASES_ESO_BAC.csv",header=T)
+datos<-datos[,2:8]
 datos$Clase<-as.factor(datos$Clase)
-datos$Curso<-as.factor(datos$Curso)
 datos$Trimestre<-as.factor(datos$Trimestre)
 #Ordenamos los niveles de clase
-datos$Clase<-factor(datos$Clase,levels=levels(datos$Clase)[c(3:13,1:2,14:17)])
-datos$Curso<-factor(datos$Curso,levels=levels(datos$Curso)[c(3:6,1:2,7:10)])
-datos1<-pivot_longer(datos,cols = 5:8,names_to = "Resultados",values_to = "numero")
+datos$Clase<-factor(datos$Clase,levels=levels(datos$Clase)[c(3:13,1:2)])
+datos$Curso<-as.factor(substr(datos$Clase,1,4))
+datos$Curso<-factor(datos$Curso,levels=levels(datos$Curso)[c(3:6,1:2)])
+datos1<-pivot_longer(datos,cols = 2:5,names_to = "Resultados",values_to = "numero")
 datos1$Resultados<-as.factor(datos1$Resultados)
 #Numero de suspensos por case y categorías (Global)
 colores<-c("#097a10","#c2af06","#f75205","#a60c25")
@@ -62,7 +59,63 @@ for(j in unique(datos1$Trimestre)){
     #ggsave(fig,file=paste(resultado,j,i,".jpeg",sep=""),height=11,width=18,dpi=180)
   }
 }
-#Para los Cursos de INFOFI
+# Realizamos as gráficas de barras para cada trimestre e cada unha das clases agrupadas por niveis
+
+## Resultados para as clases de ESO
+for(j in unique(datos1$Clase)){
+  dato<-dplyr::filter(datos1,Clase==j & numero>0)
+  dato<-dato[grep("^ESO",dato$Curso),]
+  dato_sort<-arrange(dato,Trimestre)
+  dato_cumsum<-ddply(dato_sort,.(Trimestre),transform,label_ypos=rev(cumsum(rev(numero))))
+  fig<-ggplot(dato_cumsum,aes(x=Trimestre,y=numero,group=Resultados),ylim(0,N_alumnos))+
+    geom_bar(aes(fill=Resultados),stat="identity")+theme_bw()+labs(title=paste("Resultados da clase",j))+scale_fill_manual(values=colores)+
+    geom_text(aes(y=label_ypos,label=numero),vjust=1.6,color="black",size=3.5)
+  print(fig)
+}
+
+## Resultados para as clases de BAC
+for(j in unique(datos1$Clase)){
+  dato<-filter(datos1,Clase==j & numero>0)
+  dato<-dato[grep("^BAC",dato$Curso),]
+  dato_sort<-arrange(dato,Trimestre)
+  dato_cumsum<-ddply(dato_sort,.(Trimestre),transform,label_ypos=rev(cumsum(rev(numero))))
+  fig<-ggplot(dato_cumsum,aes(x=Trimestre,y=numero,group=Resultados),ylim(0,N_alumnos))+
+    geom_bar(aes(fill=Resultados),stat="identity")+theme_bw()+labs(title=paste("Resultados da clase",j))+scale_fill_manual(values=colores)+
+    geom_text(aes(y=label_ypos,label=numero),vjust=1.6,color="black",size=3.5)
+  print(fig)
+}
+
+
+
+
+
+
+
+
+
+#Para los Cursos de INFOFI E PEIEST
+
+#Importamos los datos
+datos<-read.csv2("CLASES_INFOFI_PEIEST.csv",header=T)
+datos<-datos[,2:7]
+datos$Clase<-as.factor(datos$Clase)
+datos$Trimestre<-as.factor(datos$Trimestre)
+#Ordenamos los niveles de clase
+datos$Clase<-factor(datos$Clase)
+datos$Curso<-as.factor(substr(datos$Clase,1,7))
+datos$Curso<-factor(datos$Curso)
+datos1<-pivot_longer(datos,cols = 2:4,names_to = "Resultados",values_to = "numero")
+datos1$Resultados<-as.factor(datos1$Resultados)
+#Numero de suspensos por case y categorías (Global)
+colores<-c("#097a10","#c2af06","#f75205","#a60c25")
+#Graficamos los cursos por trimestres
+for(i in unique(datos1$Trimestre)){
+  datos<-dplyr::filter(datos1,Trimestre==i)
+  grafica<-ggplot(data=datos,aes(x=Curso,y=numero,fill=Resultados))+geom_bar(stat="identity",position=position_dodge())+
+    theme_grey()+labs(title=paste("Resultados de los diferentes cursos trimestre ",i))+scale_fill_manual(values=colores)
+  print(grafica)
+}
+#GRAFICAMOS POR CLASE PARA INFOFI
 for(j in unique(datos1$Trimestre)){
   dato<-filter(datos1,Trimestre==j)
   dato<-dato[grep("^INF",datos1$Curso),]
@@ -77,7 +130,7 @@ for(j in unique(datos1$Trimestre)){
   }
 }
 
-#Para los Cursos de PEIEST
+#GRAFICAMOS POR CLASE PARA PEIEST
 for(j in unique(datos1$Trimestre)){
   dato<-filter(datos1,Trimestre==j)
   dato<-dato[grep("^PEI",datos1$Curso),]
@@ -107,5 +160,5 @@ for(j in unique(datos1$Clase)){
 }
   
 
-grep("^ESO",datos1$Curso)
+
 
